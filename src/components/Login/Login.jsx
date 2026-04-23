ae93166a764c303298bd9bb4f4258d2d935d5d1d
tushar387 <thusharshetty8@gmail.com>
2026-04-23 14:47:18 +0530

    refator: fixed the css conflits

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import LoginHeader from './LoginHeader';
 
 const Login = () => {
   return (
     <>
     <Navbar/>
-    {/* <div className="container d-flex justify-content-center align-items-center vh-100">
+    <div className="container d-flex justify-content-center align-items-center vh-100">
 
-    </div> */}
+        <div className='card shadow login-card'>
+
+            <LoginHeader/>
+
+
+        </div>
+
+    </div>
     
     </>
   )
---
+++ src/components/Login/LoginHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoginHeader = () => {
+  return (
+    <div className='text-center mb-4'>
+        <h2>Login</h2>
+        <p>Hi 👋, Sign in to vote on campus polls </p>
+    </div>
+  )
+}
+
+export default LoginHeader
